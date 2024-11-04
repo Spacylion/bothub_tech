77b9f04638520c97fbcff01de47c156bfbb8b37a
German Kosach <fuerstein@yandex.ru>
2024-11-04 21:52:36 +0300

    feat: throttler attempts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { configurationValidationSchema } from './config/configuration';
 import { RepositoriesModule } from './db/repository/repositories.module';
 import { UsersService } from './db/service/users.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './db/service/prisma.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -15,10 +15,23 @@ import { PrismaService } from './db/service/prisma.service';
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       validationSchema: configurationValidationSchema,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 10,
+        limit: 3,
+      },
+    ]),
     RepositoriesModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, UsersService, PrismaService],
+  controllers: [],
+  providers: [
+    UsersService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,19 +3,28 @@ import {
   Controller,
   HttpException,
   HttpStatus,
+  Logger,
   Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Throttle } from '@nestjs/throttler';
+import { ATTEMPTS_LIMIT, TTL_RATE_LIMIT_IN_MSEC } from '../../shared/constants';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(this.constructor.name);
+
   constructor(private readonly authService: AuthService) {}
 
   @ApiOperation({ summary: 'Register a new user' })
   @ApiResponse({ status: 201, description: 'User successfully registered.' })
   @ApiResponse({ status: 400, description: 'Username already taken.' })
+  @ApiResponse({
+    status: 400,
+    description: 'Password does not meet requirements.',
+  })
   @ApiBody({
     schema: {
       type: 'object',
@@ -28,18 +37,50 @@ export class AuthController {
   })
   @Post('register')
   async register(@Body() body: { username: string; password: string }) {
-    return this.authService.register(body.username, body.password);
+    try {
+      return await this.authService.register(body.username, body.password);
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      } else {
+        throw new HttpException(
+          'Internal server error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 
-  @ApiOperation({ summary: 'Login user' })
-  @ApiResponse({ status: 200, description: 'Login successful.' })
-  @ApiResponse({ status: 401, description: 'Invalid credentials.' })
+  @Throttle({ default: { limit: ATTEMPTS_LIMIT, ttl: TTL_RATE_LIMIT_IN_MSEC } })
+  @ApiOperation({
+    summary: 'User login',
+    description: `Limits: ${ATTEMPTS_LIMIT} requests per ${TTL_RATE_LIMIT_IN_MSEC / 1000} seconds`,
+  })
   @Post('login')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string' },
+        password: { type: 'string' },
+      },
+      required: ['username', 'password'],
+    },
+  })
+  @ApiResponse({ status: 200, description: 'Successfully logged in' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many requests, please try again later',
+  })
   async login(@Body() body: { username: string; password: string }) {
+    console.log('Login attempt:', body);
     const token = await this.authService.login(body.username, body.password);
     if (!token) {
+      console.log('Invalid credentials');
       throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
     }
+    console.log('Generated token:', token);
     return token;
   }
 }
